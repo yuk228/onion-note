dcfe73b1f4f994aefbad4bc74e5377a199db254e
Yuk <194586153+yuk228@users.noreply.github.com>
2025-06-29 14:56:13 +0900

    🎨 Prettier Format

--- components/layouts/footer.tsx
+++ components/layouts/footer.tsx
@@ -5,8 +5,18 @@ const Footer = () => {
   return (
     <footer>
       <div className="flex flex-row justify-center gap-4 text-muted-foreground">
-        <Link href="/legal/terms" className="hover:text-foreground hover:underline transition-colors">Terms of Service</Link>
-        <Link href="/legal/privacy" className="hover:text-foreground hover:underline transition-colors">Privacy Policy</Link>
+        <Link
+          href="/legal/terms"
+          className="hover:text-foreground hover:underline transition-colors"
+        >
+          Terms of Service
+        </Link>
+        <Link
+          href="/legal/privacy"
+          className="hover:text-foreground hover:underline transition-colors"
+        >
+          Privacy Policy
+        </Link>
       </div>
       <p className="text-center py-4 text-muted-foreground">
         Copyright © 2025 Onion Note | All Rights Reserved
